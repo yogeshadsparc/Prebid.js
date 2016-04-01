d75a257e13caee48904a6bee6d14752373499b1a
protonate <ncozi@appnexus.com>
2016-03-31 22:09:00 -0700

    Prebid Analytics API
    Provides two new Prebid API methods to register analytics adapters and enable analytics tracking for any analytics module.
    * `pbjs.enableAnalytics()` is called once on the page after all analytics libraries have been loaded
    * `pbjs.registerAnalyticsAdapter` is available to register an adapter dynamically (optional)
    
    An analytics provider can be configured with these steps:
    
    1. Add the analytics library to your page as described in the provider's documentation (see `integrationExamples/gpt/examples...` for Google Analytics.)
    1. Add a call to `pbjs.enableAnalytics(analyticsAdapters)`. `analyticsAdapters` could look like:
    ```
    pbjs.que.push(function () {
          pbjs.enableAnalytics([{
              provider: 'ga',
              options: {
                  global: 'ga',
                  enableDistribution: false
    
              }
          }, {
                  provider: 'example',
                  options: {
                          foo: 1234
                  }
          }
          ]);
      });
    ```
    1. Create an analytics adapter to listen for Prebid events and call the analytics library (see `src/adapters/analytics/ga.js`)
    1. Add the analytics adapter file name to the "analytics" array in `package.json`.
    
    * review notes: restore `enableAnalytics` method name, prevent duplicate calls to adapter.enableAnalytics
    * set analytics example libraries url to fqdn localhost:9999
    * call _enqueue from containing scope
    * fix bug `this` undefined
    * Add `analyticsType` as 'library' or 'endpoint' and provide an `ajax` module to use when sending event payloads to an endpoint

---
+++ src/adapters/analytics/AnalyticsAdapter.js
@@ -0,0 +1,113 @@
+import CONSTANTS from 'src/constants.json';
+import { loadScript } from 'src/adloader';
+import { ajax } from 'src/ajax';
+
+const events = require('src/events');
+const utils = require('../../utils');
+
+const BID_REQUESTED = CONSTANTS.EVENTS.BID_REQUESTED;
+const BID_TIMEOUT = CONSTANTS.EVENTS.BID_TIMEOUT;
+const BID_RESPONSE = CONSTANTS.EVENTS.BID_RESPONSE;
+const BID_WON = CONSTANTS.EVENTS.BID_WON;
+const LIBRARY = 'library';
+const ENDPOINT = 'endpoint';
+
+var _timedOutBidders = [];
+
+export default function AnalyticsAdapter({ url, analyticsType, global, handler }) {
+  var _queue = [];
+  var _eventCount = 0;
+  var _enableCheck = true;
+
+  if (analyticsType === 'library') {
+    loadScript(url, _emptyQueue);
+  }
+
+  if (analyticsType === 'endpoint') {
+    _emptyQueue();
+  }
+
+  return {
+    track: _track,
+    enqueue: _enqueue,
+    enableAnalytics: _enable,
+    getAdapterType: () => analyticsType
+  };
+
+  function _track({ eventType, args }) {
+    if (this.getAdapterType() === LIBRARY) {
+      window[global](handler, eventType, args);
+    }
+
+    if (this.getAdapterType() === ENDPOINT) {
+      _callEndpoint(...arguments);
+    }
+  }
+
+  function _callEndpoint({ eventType, args, callback }) {
+    ajax(url, callback, JSON.stringify({ eventType, args }));
+  }
+
+  function _enqueue({ eventType, args }) {
+    const _this = this;
+
+    if (global && window[global] && eventType && args) {
+      this.track({ eventType, args });
+    } else {
+      _queue.push(function () {
+        _eventCount++;
+        _this.track({ eventType, args });
+      });
+    }
+  }
+
+  function _enable() {
+    var _this = this;
+
+    //first send all events fired before enableAnalytics called
+    events.getEvents().forEach(event => {
+      if (!event) {
+        return;
+      }
+
+      const { eventType, args } = event;
+
+      if (eventType === BID_TIMEOUT) {
+        _timedOutBidders = args.bidderCode;
+      } else {
+        _enqueue.call(_this, { eventType, args });
+      }
+    });
+
+    //Next register event listeners to send data immediately
+
+    //bidRequests
+    events.on(BID_REQUESTED, args => this.enqueue({ eventType: BID_REQUESTED, args }));
+    events.on(BID_RESPONSE, args => this.enqueue({ eventType: BID_RESPONSE, args }));
+    events.on(BID_TIMEOUT, args => this.enqueue({ eventType: BID_TIMEOUT, args }));
+    events.on(BID_WON, args => this.enqueue({ eventType: BID_WON, args }));
+
+    // finally set this function to return log message, prevents multiple adapter listeners
+    this.enableAnalytics = function _enable() {
+      return utils.logMessage(`Analytics adapter for "${global}" already enabled, unnecessary call to \`enableAnalytics\`.`);
+    };
+  }
+
+  function _emptyQueue() {
+    if (_enableCheck) {
+      for (var i = 0; i < _queue.length; i++) {
+        _queue[i]();
+      }
+
+      //override push to execute the command immediately from now on
+      _queue.push = function (fn) {
+        fn();
+      };
+
+      //turn check into NOOP
+      _enableCheck = false;
+    }
+
+    utils.logMessage(`event count sent to ${global}: ${_eventCount}`);
+  }
+}
